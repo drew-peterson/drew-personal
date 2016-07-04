94bb8248691f1e2eabbf695d99d3c7cac1549d99
weeznog <drew.m.peterson@gmail.com>
2016-07-03 17:37:55 -0700

    transition to fixed complete

--- app/src/components/header/main/mainHeader.component.js
+++ app/src/components/header/main/mainHeader.component.js
@@ -1,12 +1,34 @@
 angular.module('MainHeader')
-.directive('mainHeader',function(){ 
+.directive('mainHeader',function($animate){ 
 	return {
 		replace: true,
 		transclude: true,
+		controllerAs: 'MH',
+		templateUrl: '../views/mainHeader.html',
 		controller: function(){
 			var MH = this;
+			MH.trigger;
 		},
-		controllerAs: 'MH',
-		templateUrl: '../views/mainHeader.html' 
+		link: function(scope, elem, attrs, MH){
+			window.addEventListener('scroll', function(){
+				var pos = document.body.scrollTop;
+				var social = $('.headerSocial');
+				console.log(pos)
+				// trigger bar
+				if(pos >= 270){
+					// social.addClass('fixed');
+					// $animate.addClass(social,'fixed');
+					scope.$apply(function(){
+						MH.trigger = true;
+					})
+				}else{
+					// social.removeClass('fixed');
+					// $animate.removeClass(social,'fixed');
+					scope.$apply(function(){
+						MH.trigger = false;
+					});
+				}
+			})
+		}
 	}
 })
