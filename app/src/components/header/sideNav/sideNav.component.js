9fe68809d1f757185d8698b7e0570a23ea723a22
weeznog <drew.m.peterson@gmail.com>
2016-07-08 16:34:03 -0700

    added css and js to sidenav

--- app/src/components/header/sideNav/sideNav.component.js
+++ app/src/components/header/sideNav/sideNav.component.js
@@ -1,5 +1,5 @@
 angular.module('MainHeader')
-.directive('sideNav', function(){
+.directive('sideNav', function($animate){
 	return { 
 		replace: true,
 		templateUrl: '/../views/sideNav.public.html',
@@ -7,12 +7,45 @@ angular.module('MainHeader')
 		controller: function(){
 
 			this.contacts = [
-				{icon: 'L', link: 'linkedin.com/in/drewmpeterson'},
-				{icon: 'G', link: 'github.com/weeznog '},
-				{icon: 'E', link: 'drew.m.peterson@gmail.com'},
-				{icon: 'P', link: '425-765-9911'}
+				{icon: '../images/linkedin.png', link: 'linkedin.com/in/drewmpeterson'},
+				{icon: '../images/github.png', link: 'github.com/weeznog '},
+				{icon: '../images/email.png', link: 'drew.m.peterson@gmail.com'},
+				{icon: '../images/phone.png', link: '425-765-9911'}
 			];
 		},
-		link: function(){}
+		link: function(scope, elem, attrs, SN){
+			var close = $('#sideNav .close');
+			var open = $('#sideNav .open');
+			var sideNav = $('#sideNav .contentWrap');
+			var mask = $('#sideNavMask');
+			var body = $('body');
+
+			// close sideNav
+			close.on('click', function(){
+				closeSideNav();
+			});
+
+			// open sideNAV
+			open.on('click', function(){
+				body.css('overflow', 'hidden');
+
+				scope.$apply(function(){
+					$animate.removeClass(sideNav, 'hide');
+					$animate.removeClass(mask, 'hide');
+				});
+
+				mask.on('click', function(){
+					closeSideNav()				
+				})
+			});
+
+			function closeSideNav(){
+				body.css('overflow', 'auto');
+				scope.$apply(function(){
+					$animate.addClass(sideNav, 'hide');
+					$animate.addClass(mask, 'hide');
+				});
+			}
+		}
 	};
 })
