b14cbc1b76a9271a65ff818895843fad9e373004
weeznog <drew.m.peterson@gmail.com>
2016-07-14 12:54:24 -0700

    remove https from url link

--- app/src/components/projects/_index.project.js
+++ app/src/components/projects/_index.project.js
@@ -4,8 +4,8 @@ angular.module('Projects',[])
 				{
 					img: '../images/wholesale.png',
 					name: 'Pacifica Coffee Wholesale',
-					url: 'https://pacifica-coffee.herokuapp.com',
-					github:'https://github.com/weeznog/pacifica-coffee-wholesale',
+					url: 'pacifica-coffee.herokuapp.com',
+					github:'github.com/weeznog/pacifica-coffee-wholesale',
 					description: 'Small coffee roaster with a focus on wholesale customers only.',
 					tags: ['Angular', 'Node', 'Express', 'MongoDB', 'Gulp', 'Sass'],
 					status: ['Work In Progress','wip']
@@ -13,8 +13,8 @@ angular.module('Projects',[])
 				{
 					img: '../images/lafayettefashionista.jpg',
 					name: 'Lafayete Fashionista',
-					url: 'https://www.pacifica-coffee.herokuapp.com',
-					github: 'https://github.com/weeznog/pacifica-coffee-wholesale',
+					url: 'pacificacoffee.herokuapp.com',
+					github: 'github.com/weeznog/pacifica-coffee-wholesale',
 					description: 'Informational website with admin portal & bag',
 					tags: ['Angular', 'Node', 'Express', 'Pintest Api', 'Gulp', 'Sass'],
 					status: ['Complete','complete']
@@ -22,8 +22,8 @@ angular.module('Projects',[])
 				{
 					img: '../images/pacifica.png', 
 					name: 'Pacifica Coffee',
-					url: 'http://pacifica.herokuapp.com/',
-					github:'https://github.com/weeznog/pacifica-coffee',
+					url: 'pacifica.herokuapp.com/',
+					github:'github.com/weeznog/pacifica-coffee',
 					description: 'Informational website with admin portal & bag',
 					tags: ['Rails', 'Ruby', 'Sass', 'JQuery', 'PostgresDB'],
 					status: ['Canceled','canceled']
@@ -36,8 +36,8 @@ angular.module('Projects',[])
 				{
 					img: '../images/potato.png',
 					name: 'Potato Quick App',
-					url: 'https://potato-quick-app.herokuapp.com/',
-					github:'https://github.com/weeznog/potato-quick-app',
+					url: 'potato-quick-app.herokuapp.com/',
+					github:'github.com/weeznog/potato-quick-app',
 					description: 'Coding Challenge for Potato, use Flickr Api',
 					tags: ['Angular', 'Node', 'Express', 'Flickr Api', 'Gulp', 'Sass'],
 					status: ['Complete','complete']
@@ -45,8 +45,8 @@ angular.module('Projects',[])
 				{
 					img: '../images/onhub.png',
 					name: 'On Hub Clone',
-					url: 'https://onhub-clone.herokuapp.com/',
-					github:'https://github.com/weeznog/onhub-clone',
+					url: 'onhub-clone.herokuapp.com/',
+					github:'github.com/weeznog/onhub-clone',
 					description: 'Clone of google product OnHub',
 					tags: ['Angular', 'Node', 'Express', 'Gulp', 'Sass'],
 					status: ['Work In Progress', 'wip']
@@ -55,7 +55,7 @@ angular.module('Projects',[])
 					img: '../images/personal.png',
 					name: 'Personal Website',
 					url: 'drewpeterson.me',
-					github:'https://github.com/weeznog/drew-personal',
+					github:'github.com/weeznog/drew-personal',
 					description: 'My Personal Website, showing off my best work!!!',
 					tags: ['Angular', 'Node', 'Express', 'Gulp', 'Sass'],
 					status: ['Work In Progress','wip']
