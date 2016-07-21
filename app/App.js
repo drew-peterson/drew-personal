ac6ee129d6f95d6c90ea9f0fb373141c3caa7c7e
weeznog <drew.m.peterson@gmail.com>
2016-07-21 09:25:25 -0700

    removed ui-router

--- app/App.js
+++ app/App.js
@@ -1,7 +1,15 @@
-'use strict';
-angular.module('DrewPersonalSiteApp',
-	[
-	'stateRouter', 
-	'MainHeader',
-	'ngAnimate'
-	]);   
+(function(){
+	'use strict';
+
+	var app = angular.module('DrewPersonalSiteApp',
+		[
+		'MainHeader',
+		'ngAnimate'
+		]
+	);   
+
+	app.component('mainContent',{
+		templateUrl: '../views/main.html'
+	});
+
+})();
