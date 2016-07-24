7f78c8ba795864be14d92f88d671804a053fc908
weeznog <drew.m.peterson@gmail.com>
2016-07-24 09:11:53 -0700

    fix showScroll

--- app/src/components/header/scrollHeader/scrollHeader.component.js
+++ app/src/components/header/scrollHeader/scrollHeader.component.js
@@ -1,53 +1,54 @@
-(function(){
+// (function(){
 	'use strict';
 
 	var nav = angular.module('Navigation');
 
 	nav.component('scrollHeader', {
 		templateUrl: '../views/scrollHeader.public.html',
-		controller: scrollCtrl,
-		controllerAs: 'SH'
+		controller: scrollCtrl
 	});
 
 	function scrollCtrl($scope, $element){
-		var self = this;
-		self.showScroll;
-		self.$postLink = $postLink;
 
-		console.log($element);
+		var self = this;
+		self.$postLink = $postLink;
 
 		// dom manipulation in angular 1.5;
 		function $postLink(){
-			window.addEventListener('scroll', function(){
+			var contact = $('#sideNav .open');
+
+			var onScroll = function(){
 				var pos = document.body.scrollTop;
-				var contact = $('#sideNav .open');
+				$scope.$apply(function(){
+					if(pos >= 270){
+						if(!self.show){
+							contact.addClass('scrolled');
+							self.show = true;
+						}
+					}else{
+						if(self.show){
+							contact.removeClass('scrolled');
+							self.show = false;
+						}
+					}		
+				}); // -apply
+			}; // -onScroll
 
-				// scroll almost below grayscale bar header
-				if(pos >= 270){
-					contact.addClass('scrolled');
-			
-					if(checkClass()){
-						self.showScroll = true;
-					};
-				}else if(pos <= 270){
-					contact.removeClass('scrolled');
+			// event handler attached manually need to force digest for var to update
+			$(window).on('scroll', onScroll);
 
-					if(!checkClass()){
-						self.showScroll = false;
-					};
-				}
-
-				function checkClass(){
-					var contact = $('#sideNav .open');
-					return contact.hasClass('scrolled');
-				};
+			// manually bond listeners need to be removed
+			$scope.$on('$destroy', function(){
+				$(window).off('scroll', onScroll);
+				console.log('remove listener');
 			});
 		}
-	};
-})();
+	}; //-showCtrl
+// })();
 
 
 
 
 
 
+// 
